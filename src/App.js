9a8e93475e8a15f2f82dcf674d7ac064781056a6
FernandoLelis <fernandolelisdarocha@gmail.com>
2021-12-21 14:31:37 -0300

    Criação do formulário com stilização utilização de bootstrap

--- src/App.js
+++ src/App.js
@@ -1,50 +1,73 @@
 import UIContainer from "./Components/UI/Container/Container";
 import AnuncioCard from "./Components/Anuncio/Card/Card";
+import Form from "./Components/Form/Form";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+
+  const [modal, setModal] = useState(false);
+
+  const abrirModal = () => {
+    setModal(!modal);
+  };
 
   const anuncio = {
     "id": 1,
     "title": "Suporte para Celular Ajustável de Metal PH36 Multifuncional",
     "imageUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDUqYAiDXWe9PRTFughuuX0hIQCqFgSLbpDA&usqp=CAU",
-    "estoque": 0,
-    "custo": 0,
-    "custoUnit": 0,
+    "stock": 3,
+    "custo": 170,
     "categoria": "Celulares e SmartFones",
     "tipo": "Clássico",
-    "taxaImpot": 0,
-    "custoFrete": 0,    
-    "porcentagemLucro": 0,
-    "price": 0,
+    "taxaImport": 0,
+    "custoFrete": 20,    
+    "margemLucro": 0,
     "lucroBrt": 0,
     "lucroLqd": 0
   };
 
   return (
-   <>
-     <header>
-      <UIContainer>
-        <div>
-        <h1>Meus Anúncios ML</h1>
-        </div>        
-      </UIContainer>       
-     </header>     
-     <section>
-      <UIContainer>
-        <div className="sectionBox">
-          <AnuncioCard anuncio={anuncio} />
+    <div className="App">
+      {
+        modal ?
+        <div className="modal"> 
+          <div className="modalContent">
+            <h3>Minha Modal</h3>
+          </div>
         </div>
-        
-      </UIContainer>
-     </section>
+        :
+        <div></div>
+      } 
+      <header>
+        <UIContainer>
+          <div>
+            <h1>Meus Anúncios ML</h1>              
+          </div>                        
+        </UIContainer>       
+      </header>     
+      <section>
+        <UIContainer>
+          <div className="sectionBox">
+            <div className="row mb-3">
+              <div className="col-sm-10">
+                <input type="search" className="form-control form-control-sm" placeholder="Buscar" />
+              </div>
+              <div className="col-sm">
+                <button type="button" className="btn btn-primary btn-sm"  onClick={() => abrirModal()}>Novo Anúncio</button>
+              </div>
+            </div>  
+            <div>
+              <AnuncioCard anuncio={anuncio} />
+            </div>
+          </div>        
+        </UIContainer>
+      </section>
       <footer>
-      <UIContainer>
-        Footer
-      </UIContainer>
-      </footer>    
-     
-   </>
+        <UIContainer>
+        </UIContainer>      
+      </footer>   
+    </div>
   );
 }
 
--- src/Components/Anuncio/Card/Card.js
+++ src/Components/Anuncio/Card/Card.js
@@ -3,30 +3,33 @@ import "./Card.css";
 
 
 export default function AnuncioCard({ anuncio }) {
+
     return(
-        <div className="anuncioCard">
-           
-            <img src={anuncio.imageUrl}  className="anuncioCard-image"/>    
-           
-            <div className="anuncioCard-info">
-                <h1 className="anuncioCard-title">{anuncio.title}</h1>
-                <span className="anuncioCard-categoria">{anuncio.categoria}</span>
-                <footer className="anuncioCard-footer">
-                    <div className="anuncioCard-price">
-                        <p>Preço</p>  
-                        R$ {anuncio.price.toFixed(2)}                    
-                    </div>
-                    <div className="anuncioCard-lucroBrt">
-                        <p>Lucro Bruto</p>
-                        R$ {anuncio.lucroBrt.toFixed(2)}                        
-                    </div>
-                    <div className="anuncioCard-lucroLqd">
-                        <p>Lucro Liquito</p>
-                        R$ {anuncio.lucroLqd.toFixed(2)}                        
-                    </div>
-                </footer>
+        <div className="card">
+            <div className="anuncioCard">            
+                <img src={anuncio.imageUrl} className="anuncioCard-image"/>               
+                <div className="anuncioCard-info">
+                    <h4 className="card-title">{anuncio.title}</h4>
+                    <footer className="anuncioCard-footer">
+                        <div className="anuncioCard-price">
+                            <p>Vender:</p>  
+                            R$ {
+                                (
+                                    0
+                                ).toFixed(2)
+                            }                   
+                        </div>
+                        <div className="anuncioCard-lucroBrt">
+                            <p>Receber:</p>
+                            R$ {anuncio.lucroBrt.toFixed(2)}                        
+                        </div>
+                        <div className="anuncioCard-lucroLqd">
+                            <p>Ganhar:</p>
+                            R$ {anuncio.lucroLqd.toFixed(2)}                        
+                        </div>
+                    </footer>
+                </div>
             </div>
-           
-        </div>
+        </div>     
     );
 };
